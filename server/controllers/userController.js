969ba0da2fbd890f9123f12329004e4a3a6f5624
MidnightTundra <chrisscotthw@gmail.com>
2023-09-10 02:56:53 -0400

    add fr sunday

---
+++ server/controllers/userController.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+let userSchemaRoute = require('../models/users.model.js');
+
+// Defines our starting point 
+router.route('/').get((req,res) => {
+    userSchemaRoute.find().then(userdata => res.json(userdata))
+    .catch(err =>   res.status(400).json('Error in retrieving data. Error code: ' + err))
+});
+
+//Creates a new entry to the database for user data based on the Schema fields.
+//This setup is for users.
+router.route('/add').post((req, res) => {
+    const userID = req.body.userID;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const address = req.body.address;
+    const zipcode = req.body.zipcode;
+
+    const newUser = new userSchemaRoute ({
+        userID, firstName, lastName, address, zipcode,
+        
+    });
+    
+    newUser.save()
+        .then(() => res.json('data entry added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+});
+
+module.exports = router;    
